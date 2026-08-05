abc04ca2441ef47c3ad371a7d88cae0fa2f31df6
achente1 <devanshjana24@gmail.com>
2026-08-05 23:35:09 +0530

    fix: disable luarocks in lazy.nvim config

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -30,6 +30,9 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
+  rocks = {
+    enabled = false, -- Disables luarocks check & silences healthcheck warnings
+  },
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
